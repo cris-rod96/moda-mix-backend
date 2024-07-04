553658f871c8c566ee6953d573ae40d1c1d89d05
Cristhian Rodríguez <crisrodam1996@gmail.com>
2024-07-04 00:35:38 -0500

    Product Model

--- src/models/index.models.js
+++ src/models/index.models.js
@@ -1,4 +1,5 @@
 import CategoryModel from './category/category.model.js';
+import ProductModel from './product/product.model.js';
 import SupplierModel from './supplier/supplier.model.js';
 import UserModel from './user/user.model.js';
-export { CategoryModel, SupplierModel, UserModel };
+export { CategoryModel, ProductModel, SupplierModel, UserModel };
--- src/models/product/product.model.js
+++ src/models/product/product.model.js
@@ -1,40 +1,85 @@
-import { Sequelize, DataTypes } from 'sequelize';
-import { BRANDS } from '../../data/enums.js';
+import { DataTypes, Sequelize } from 'sequelize';
+import { BRANDS, COLORS } from '../../data/enums.js';
 
-const ProductModel = () => {
-  new Sequelize().define('Product', {
-    id: {
-      type: DataTypes.UUID,
-      primaryKey: true,
-      defaultValue: DataTypes.UUIDV4,
-    },
-    name: {
-      type: DataTypes.UUID,
-      allowNull: false,
-      unique: true,
-    },
-    price: {
-      type: DataTypes.FLOAT,
-      allowNull: false,
-      validate: {
-        min: 1.0,
+const ProductModel = (conn) => {
+  conn.define(
+    'Product',
+    {
+      id: {
+        type: DataTypes.UUID,
+        primaryKey: true,
+        defaultValue: DataTypes.UUIDV4,
+      },
+      name: {
+        type: DataTypes.UUID,
+        allowNull: false,
+        unique: true,
+      },
+      price: {
+        type: DataTypes.FLOAT,
+        allowNull: false,
+        validate: {
+          min: 1.0,
+        },
+      },
+      stock: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      brand: {
+        type: DataTypes.ENUM,
+        allowNull: false,
+        values: BRANDS,
+      },
+      sizes: {
+        type: DataTypes.ARRAY(DataTypes.STRING),
+        allowNull: false,
+      },
+      colors: {
+        type: DataTypes.ARRAY(DataTypes.STRING),
+        allowNull: false,
+        validate: {
+          isValidColors: (values) => {
+            if (!Array.isArray(values))
+              throw new Error('Formato de colores incorrecto.');
+
+            const foundColores = values.every((value) =>
+              COLORS.includes(value)
+            );
+            if (!foundColores) throw new Error('Hay colores no válidos');
+          },
+        },
+      },
+      images: {
+        type: DataTypes.ARRAY(DataTypes.STRING),
+        allowNull: false,
+      },
+      status: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: true,
+      },
+
+      CategoryId: {
+        type: DataTypes.UUID,
+        allowNull: false,
+        references: {
+          model: 'Categories',
+          key: 'id',
+        },
+      },
+      SupplierId: {
+        type: DataTypes.UUID,
+        allowNull: false,
+        references: {
+          model: 'Suppliers',
+          key: 'id',
+        },
       },
     },
-    stock: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-    brand: {
-      type: DataTypes.ENUM,
-      allowNull: false,
-      values: BRANDS,
-    },
-    sizes: {
-      type: DataTypes.ARRAY(DataTypes.STRING),
-      allowNull: false,
-    },
-    colors: {},
-  });
+    {
+      timestamps: false,
+    }
+  );
 };
 
 export default ProductModel;
